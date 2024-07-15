ca998da381e180fdb22054a81c6e5f4b89287582
john layton <johnstewartlayton@gmail.com>
2024-07-16 04:57:18 +1000

    Initialise project

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    java
+    application
+    distribution
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.graalvm.buildtools.native)
+    alias(libs.plugins.openapi.gradle.plugin)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter)
+    if (project.hasProperty("native")) {
+        implementation(libs.aws.serverless.java.container.springboot3)
+    } else {
+        implementation(libs.spring.boot.starter.web)
+        implementation(libs.spring.boot.starter.actuator)
+        implementation(libs.springdoc.openapi.starter.webmvc.ui)
+    }
+    testImplementation(libs.spring.boot.starter.test)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+application {
+    mainClass = "org.orrlane.DemoApplication"
+}
+
+distributions {
+    create("custom") {
+        contents {
+            into("/")
+            from(project.layout.projectDirectory.dir("src/main/scripts"))
+            from(tasks.nativeCompile)
+        }
+    }
+}
+
+openApi {
+    outputFileName.set("openapi.yaml")
+    apiDocsUrl.set("http://localhost:8080/v3/api-docs.yaml")
+//    forkProperties = "-Dspring.profiles.active=zipkin"
+}
+
+graalvmNative {
+    binaries {
+        all {
+        }
+        named("main") {
+            buildArgs.add("--verbose")
+            buildArgs.add("--enable-preview")
+            buildArgs.add("-march=compatibility")
+            buildArgs.add("--enable-url-protocols=http")
+        }
+    }
+}
+
+task<Copy>("createFunctionZip") {
+    from(tasks.named("customDistZip"))
+    into(project.layout.buildDirectory)
+    rename { _ : String -> "function.zip" }
+}
+
+//project.tasks.fo
+
+tasks.forkedSpringBootRun {
+    dependsOn(
+        tasks.named("processAotResources"),
+        tasks.named("compileAotJava"))
+}
+
+//tasks {
+//    forkedSpringBootRun {
+//        dependsOn(tasks.named("processAotResources"))
+//        doNotTrackState("See https://github.com/springdoc/springdoc-openapi-gradle-plugin/issues/102")
+//    }
+//}
---
+++ iac/src/main/java/org/orrlane/IacStack.java
@@ -0,0 +1,62 @@
+package org.orrlane;
+
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Architecture;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.Tracing;
+import software.amazon.awscdk.services.logs.RetentionDays;
+import software.constructs.Construct;
+
+public class IacStack extends Stack {
+    public IacStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public IacStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // Define the Lambda resource
+        Function function = Function.Builder.create(this, "HelloWorldFunction")
+                .runtime(Runtime.PROVIDED_AL2023)
+                .code(Code.fromAsset(functionPath()))
+                .handler("NOP")
+                .timeout(Duration.seconds(10))
+                .memorySize(2048)
+                .logRetention(RetentionDays.ONE_WEEK)
+                .tracing(Tracing.ACTIVE)
+                .architecture(Architecture.X86_64)
+                .build();
+
+        // Define the API Gateway resource
+//                .proxy(false)
+        LambdaRestApi api = LambdaRestApi.Builder.create(this, "HelloWorldApi")
+                .handler(function)
+                .build();
+
+        // Define the '/hello' resource and its GET method
+/*
+        Resource helloResource = api.getRoot().addResource("demo");
+        helloResource.addMethod("GET");
+*/
+
+        CfnOutput.Builder.create(this, "DemoTestApiUrl")
+                .exportName("DemoTestApiUrl")
+                .value(api.getUrl())
+                .build();
+    }
+
+    public static String functionPath() {
+        /**
+         * CodeUri: ../app/target/app-0.0.0-main-SNAPSHOT-native-zip.zip
+         * CodeUri: ../app/build/distributions/app-custom-0.0.0-main-SNAPSHOT.zip
+         */
+        return "../app/build/function.zip";
+    }
+
+}
